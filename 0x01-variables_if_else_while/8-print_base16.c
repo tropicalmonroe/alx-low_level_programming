f022529088b1caa931b6d6749ccd8266f43afdec
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-08 06:53:39 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char i;
+
+for (i = 48; i <= 58; i++)
+{
+putchar(i);
+}
+
+for (i = 97; i <= 102; i++)
+{
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+
+}
+
