4ce30d975271d2680cd6054759784c41a13a1509
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-08 06:32:36 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,15 +7,15 @@
 int main(void)
 
 {
-        int i;
+int i;
 
-        for (i = 48; i <=57; i++)
-        {
-                putchar(i);
-        }
+for (i = 48; i <=57; i++)
+{
+putchar(i);
+}
 
-        putchar('\n');
+putchar('\n');
 
-        return (0);
+return (0);
 
 }
