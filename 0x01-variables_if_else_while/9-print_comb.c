4c98abfebc3d1cd2d83edac0c437026e1948c204
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-08 07:07:03 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+
+if (i == 57)
+{
+break;
+}
+
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
