18218bb5f99e75f3b5c639d5b392e8d884eed934
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-08 04:42:03 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+	char lc;
+	char uc;
+
+	for (uc = 'A'; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
