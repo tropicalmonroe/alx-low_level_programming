d3d4942fe89abeb176f15e04ecc457f6682860eb
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-12 04:23:59 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -15,7 +16,7 @@ int b;
 for (a = 0; a <= 23; a++)
 {
 for (b = 0; b <= 59; b++)
-{	
+{
 _putchar (a / 10 + '0');
 _putchar (a % 10 + '0');
 _putchar (':');
@@ -24,6 +25,7 @@ _putchar (b % 10 + '0');
 _putchar ('\n');
 
 }
+
 
 }
 
