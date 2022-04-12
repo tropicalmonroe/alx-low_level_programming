097e8c99c392ee3c351e75b2c0eff4c80e63cb5b
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-12 01:56:08 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ *@n: the int to print
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+
+{
+
+if (n > 0)
+{
+_putchar (+);
+
+return (1);
+}
+
+else if (n = 0)
+{
+_putchar (0);
+
+return (0);
+}
+
+else (n < 0)
+{
+_putchar (-);
+
+return (-1);
+}
+
+}
