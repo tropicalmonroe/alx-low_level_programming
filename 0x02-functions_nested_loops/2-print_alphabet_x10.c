41750d59f65129e1c463894427526a8b89d511bd
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-12 01:22:11 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void);
+{
+char a;
+int c = 0;
+
+while (c < 10)
+{
+for (a = 'a'; a<= 'z'; a++)
+{
+_putchar(a);
+}
+
+c++;
+_putchar('\n');
+}
+
+return(0);
+
+}
