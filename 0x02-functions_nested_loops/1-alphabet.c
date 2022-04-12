691e5ecca08d89bc0e8c17126b4a2392a919e38b
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-12 01:01:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+{
+char i;
+
+for (i='a'; i<='z'; i++)
+{
+_putchar(i);
+}
+
+_putchar('\n');
+
+return(0);
+
+}
