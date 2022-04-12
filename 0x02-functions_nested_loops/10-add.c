cf0579ac001a4e9354aa43951af6af7aeaf820ba
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-12 04:53:05 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @x: prints int x
+ * @y: prints int y
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+
+{
+
+return (x + y);
+
+}
