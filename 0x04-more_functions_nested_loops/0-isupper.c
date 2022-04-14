d3d4b30e1454d899b1119c6fea4ceedb50f86de7
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-14 03:37:53 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * isupper - checks for uppercaste character
+ * _isupper - checks for uppercaste character
  *@c: the character to be tracked 
- *Return:1 if character is uppercase and 0 if not
+ *Return: 1 if character is uppercase and 0 if not
  */
 
 int _isupper(int c)
