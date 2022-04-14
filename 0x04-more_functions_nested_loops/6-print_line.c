86c450c8912e1f6b1303f36de2d79a10f01460fb
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-14 02:07:46 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints code on straight line
+ *@n: number to print
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n;; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
