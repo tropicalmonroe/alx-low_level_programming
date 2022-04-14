9dbf5c70e6dcdd4597631f3d958aa56f0d8317ea
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-14 01:22:02 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return:0
+ */
+
+void print_most_numbers(void)
+{
+	char x = 0;
+	
+	while (x <= 9)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar('0' + x);
+		}
+
+		x++;
+	}
+	_putchar("\n");
+
+}
+
