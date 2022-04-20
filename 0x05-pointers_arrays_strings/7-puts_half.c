61c0fbc9051d5f88a4f67cdd0ab5f2a6cc7d54cc
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-20 06:08:33 -0700

    Task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,12 @@
 
 void puts_half(char *strii)
 {
-
 	int length, n, i;
 
 	for (length = 0; str[length] != '\0'; length++)
-	
+	{
+	}
 	n = (length - 1) / 2;
-
 	for (i = n + 1; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
