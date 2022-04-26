d617e8d96d32f75838bb706ced1ea6393cf0af3c
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-26 02:30:18 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Locate a string char
+ * @s: input string
+ * @c: char to find
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
