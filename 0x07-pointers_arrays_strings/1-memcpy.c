16165468a065e108badbdf64feb237c3129826d4
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-26 02:05:05 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy: 
+ * @dest: input pointers to destination
+ * @src: input pointers to sourse
+ * @n: size of bytes
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+	return (dest);
+}
