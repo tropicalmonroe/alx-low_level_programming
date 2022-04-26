7d608cbf1077c9754fbe569b6fdc7665b64f8c77
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-26 03:18:45 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - finds first matching char in string
+ * @s: input string
+ * @accept: characters matched
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
+
