193ded9c6f029b5d22909e2e0a8c6e85220e2932
tropicalmonroe <shitsamajohn821@gmail.com>
2022-04-28 03:22:48 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Length of string
+ * @s: string
+ * return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + strlen_recursion(s + 1))
+	}
+	return (0);
+}
