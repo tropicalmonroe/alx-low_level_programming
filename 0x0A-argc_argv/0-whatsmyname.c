6a0ad536473d8352a71823ab76c7efaaf8365f95
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-03 04:39:08 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main (int argc, char **argv)
+{
+	if ( argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
