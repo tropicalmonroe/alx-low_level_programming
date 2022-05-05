dbd8db6d32ddd9b501a1aad8585d24a74b116e59
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-05 02:37:02 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int i, f = 0, len = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
