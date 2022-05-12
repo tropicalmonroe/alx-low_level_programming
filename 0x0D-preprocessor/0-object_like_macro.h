1c929ef30fea74d455faa71423016ed5aa94fac3
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-12 02:29:55 -0700

    Task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,5 @@
-#ifndef FILE_MACRO
-#define FILE_MACRO
-
-/**
- * File: macro.h like
- * Desc: Size of 1024
- */
+#ifndef 0-object_like_macro.h
+#define 0-object_like_macro.h
 
 #define size 1024
 
