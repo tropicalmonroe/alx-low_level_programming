92e5eb62e74b6f11c2d2209d5bb540171d4f11c7
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-16 04:23:34 -0700

    function operators

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,68 +1,73 @@
-
-#include "3-calc.h"
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_add - the sum of two numb
- * @a: The first numb
- * @b: The second numb
- *
- * Return: The sum of a and b.
+ * op_add - addition
+ * @a: sumnd 1
+ * @b: sumnd 2
+ * Return: rsult
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - the difference of two numb
- * @a: The first numb
- * @b: The second numb
- *
- * Return: The difference of a and b.
+ * op_sub - subtraction
+ * @a: minuend
+ * @b: sustrnd
+ * Return: result
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - the multiplication of two numb
- * @a: The first numb
- * @b: The second numb
- *
- * Return: The multiplication of a and b.
+ * op_mul - multiplication
+ * @a: multp 1
+ * @b: multp 2
+ * Return: result
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - the division of two numb
- * @a: The first numb
- * @b: The second numb
- *
- * Return: The quotient of a and b.
+ * op_div - division
+ * @a: divisr
+ * @b: dividndo
+ * Return: reult
  */
+
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b)
+	{
+		return (a / b);
+	}
+	printf("Error\n");
+	exit(100);
 }
 
 /**
- * op_mod - the remainder of division of two numb
- * @a: The first numb
- * @b: The second numb
- *
- * Return: The remainder of the division of a by b
+ * op_mod - modulo
+ * @a: vaper 1
+ * @b: vaper 2
+ * Return: result
  */
+
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b)
+	{
+		return (a % b);
+	}
+	printf("Error\n");
+	exit(100);
 }
+
