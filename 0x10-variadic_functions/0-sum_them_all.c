26a983ed8b70669b58c60a3281163974394c4708
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-17 04:05:44 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - returns the sum of all its parameters
  * @n: contant unsigned int
  * @...: variadic function
- * return sum
+ * Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -19,12 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 		sum += va_arg(x, int);
 
-
-
 	va_end(x);
 
 	return (sum);
 }
-
-
-
