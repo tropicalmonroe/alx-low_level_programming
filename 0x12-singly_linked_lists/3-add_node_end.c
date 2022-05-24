69997dfe2d494c7009fe844f7d6b48293a58f973
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-24 05:22:02 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,48 +3,44 @@
 #include <string.h>
 
 /**
- * add_node_end - func that adds a new node at the beginning of a list_t
+ * add_node_end - func that adds a new node at the end of a list_t
  * @head: pointer at the beginning of the value
  * @str: pointer characters to be inserted
  * Return: NULL if func fails
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
         list_t *new, *last;
-        char *dup;
         int len;
 
-        new = malloc(sizeof(list_t));
+	if (str == NULL)
+		return (NULL);
 
-        if (new == NULL)
-                return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-        dup = strdup(str);
-
-        if (dup == NULL)
-        {
-                free(new);
-                return (NULL);
-        }
-
-        for (len = 0; str[len];)
-                len++;
-
-        new->str = dup;
-        new->len = len;
-        new->next = NULL;
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (str[len])
+		len++;
+	temp->len = len;
+	temp->next = NULL;
 
 	if (*head == NULL)
-		*head = new;
-
-	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		*head = new;
+		return (new);
 	}
 
-        return (*head);
+	last = *head;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+	return (new);
 }
