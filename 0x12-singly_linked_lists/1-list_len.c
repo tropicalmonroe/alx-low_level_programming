2e92ec3e337140cff16e0bd4e400e1d4878aa48f
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-24 04:32:00 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Finds the number of elements in linked list
+ * @h: The linked list_t list pointer
+ * @el: a counter var for (nodes)
+ * Return: The number of el
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t el = 0;
+
+	while (h)
+	{
+		el++;
+		h = h->next;
+	}
+
+	return (el);
+}
