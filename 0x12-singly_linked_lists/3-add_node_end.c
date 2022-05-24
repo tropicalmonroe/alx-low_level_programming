8bf000728c138f5a026badf055c806f990c5f5d2
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-24 05:27:22 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,18 +1,31 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node_end - func that adds a new node at the end of a list_t
- * @head: pointer at the beginning of the value
- * @str: pointer characters to be inserted
- * Return: NULL if func fails
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
  */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
 
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node_end - node in the end of linklist
+ * @head: pointer to a linklist
+ * @str: string nnode
+ * Return: new node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *new, *last;
-        int len;
+	list_t *new, *end;
 
 	if (str == NULL)
 		return (NULL);
@@ -22,25 +35,22 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
+
 	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	while (str[len])
-		len++;
-	temp->len = len;
-	temp->next = NULL;
-
+	new->len = _strlen(new->str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-
-	last = *head;
-	while (last->next)
-		last = last->next;
-	last->next = new;
+	end = *head;
+	while (end->next)
+		end = end->next;
+	end->next = new;
 	return (new);
 }
