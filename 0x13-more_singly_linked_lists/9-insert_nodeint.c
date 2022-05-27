5e7a68855257620ed6bc671e5c326fb843aa6a4b
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-27 02:09:32 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,43 +6,40 @@
  * @head: double pointer to the first node
  * @idx: index of the node
  * @n: value new node
+ *
  * Return: the indexed node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x;
-	listint_t *c;
-       	listint_t *r;
+	listint_t *now;
+       	listint_t *clean;
 
 	if (head == NULL)
 		return (NULL);
 	if (idx != 0)
 	{
-		c = *head;
-		for (x = 0; x < idx - 1 && c != NULL; i++)
+		now = *head;
+		for (x = 0; x < idx - 1 && now != NULL; x++)
 		{
-			c = c->next;
+			now = now->next;
 		}
-		if (c == NULL)
+		if (now == NULL)
 			return (NULL);
 	}
-	r = malloc(sizeof(listint_t));
-	if (r == NULL)
+	clean = malloc(sizeof(listint_t));
+	if (clean == NULL)
 		return (NULL);
-
-	r->n = n;
-
+	clean->n = n;
 	if (idx == 0)
 	{
-		r->next = *head;
-		*head = r;
-		return (r);
+		clean->next = *head;
+		*head = clean;
+		return (clean);
 	}
-
-	r->next = c->next;
-	c->next = r;
-	return (r);
+	clean->next = now->next;
+	now->next = clean;
+	return (clean);
 }
-
 
