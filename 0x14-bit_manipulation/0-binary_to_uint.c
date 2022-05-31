cd766aec94884c892f6291890c96f78727822f74
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-31 03:53:27 -0700

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - a function that converts a binary number
+ * @b: pointer construct character containg bin num
+ *
+ * Return: 0 if error
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	int x;
+
+	i = 0;
+
+	if (!b)
+		return (0);
+
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+	}
+
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		i <<= 1;
+		if (b[x] == '1')
+			i += 1;
+	}
+
+	return (i);
+}
