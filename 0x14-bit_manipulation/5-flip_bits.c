abf5941dfd67f70882364f502bab20dec5e09fc6
tropicalmonroe <shitsamajohn821@gmail.com>
2022-05-31 05:23:46 -0700

    Task 4

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  flip_bits - function that returns the number of bits you would need to flip
+ *  @n: pointer number of bits
+ *  @m: 2nd number
+ *  Return: bits flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int d;
+	unsigned long int last;
+	unsigned int first;
+	unsigned int y;
+
+	last = 0;
+	first = 1;
+
+	d = n ^ m;
+
+	for (y = 0; y < (sizeof(unsigned long int) * 8); y++)
+	{
+		if (last == (d & last))
+			first++;
+
+		last <<= 1;
+	}
+
+	return (first);
+}
