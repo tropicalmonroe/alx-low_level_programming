74bff3c69cc537390a5cedcce36b746f32015d96
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-02 03:18:49 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	andika = write(fungua, text_content, urefu);
 
 	if (fungua == -1 || andika == -1)
-		return (1);
+		return (-1);
 
 	close(fungua);
 
