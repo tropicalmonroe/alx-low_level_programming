020fdf83d1e4b3f79ec056d34c21cfb5213e389c
tropicalmonroe <shitsamajohn821@gmail.com>
2022-06-02 03:13:30 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -41,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	rite = write(STDOUT_FILENO, buf, letters);
+	rite = write(STDOUT_FILENO, buf, reed);
 	free(buf);
 
 	if (reed != rite)
