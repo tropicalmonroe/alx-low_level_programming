1a27aae688a4903d9aaca54c2416e72a3c187fcd
tropicalmonroe <shitsamajohn821@gmail.com>
2022-07-07 03:50:10 -0700

    Mandatory task fix 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,22 @@
 
 size_t print_dlistint(const dlistint_t *h);
 {
-	size_t con = 0;
+	int count;
+
+	count = 0;
 
 	if (h == NULL)
-		return (0);
+		return (count);
 
-	while (h)
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		con++;
+		count++;
 		h = h->next;
 	}
 
-	return (con);
+	return (count);
 }
